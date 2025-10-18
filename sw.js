efda8f3b11850d5ac8823a182db2155bee66b6c2
Masato-Nasu <1bitexist@gmail.com>
2025-10-18 13:40:11 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 // sw.js — Motion Theremin PWA — 202510180427
-const SW_VERSION = "theremin-20251018043543";
+const SW_VERSION = "theremin-20251018043920";
 const CACHE = "pwa-cache-" + SW_VERSION;
 
 // 必ず更新させたいアセット
 const ASSETS = [
   "./",
-  "./index.html?v=20251018043543",
-  "./manifest.json?v=20251018043543",
-  "./icon-192.png?v=20251018043543",
-  "./icon-512.png?v=20251018043543"
+  "./index.html?v=20251018043920",
+  "./manifest.json?v=20251018043920",
+  "./icon-192.png?v=20251018043920",
+  "./icon-512.png?v=20251018043920"
 ];
 
 self.addEventListener("install", (e) => {
