4351936c71328c7245b7177a91fa3912810062b2
Masato-Nasu <1bitexist@gmail.com>
2025-10-18 13:02:28 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// sw.js v2
-const CACHE="kbxaerial-full-v2";
-const ASSETS=["./","./index.html?v=2","./manifest.json","./icon-192.png","./icon-512.png"];
+// sw.js v4
+const CACHE="kbxaerial-full-v4";
+const ASSETS=["./","./index.html?v=4","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",e=>{
