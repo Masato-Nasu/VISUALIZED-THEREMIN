d4557368e914008c51fc6ec940e0e036d0e8af19
Masato-Nasu <1bitexist@gmail.com>
2025-10-18 13:20:57 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// sw v1
-const CACHE="kb-theremin-edgeswipe-v1";
-const ASSETS=["./","./index.html?v=1","./manifest.json","./icon-192.png","./icon-512.png"];
+// sw v3 (no mic)
+const CACHE="kb-theremin-edgeswipe-v3";
+const ASSETS=["./","./index.html?v=3","./manifest.json","./icon-192.png","./icon-512.png", "./Chime.mp3"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",e=>{
