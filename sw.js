995364123a43687b88313f15638537ff9ef71427
Masato-Nasu <1bitexist@gmail.com>
2025-10-18 14:56:59 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
-// sw.js v1.4.0
-const CACHE='baby-theremin-v1-4-0';
-const ASSETS=['./','./index.html?v=1.4.0','./manifest.json?v=1.4.0','./icon-192.png','./icon-512.png','./Chime.mp3'];
+// sw.js v1.5.0
+const CACHE='baby-theremin-v1-5-0';
+const ASSETS=['./','./index.html?v=1.5.0','./manifest.json?v=1.5.0','./icon-192.png','./icon-512.png','./Chime.mp3'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim();});
-self.addEventListener('fetch',e=>{const req=e.request; if(req.mode==='navigate'){e.respondWith(fetch(req).catch(()=>caches.match('./index.html?v=1.4.0')));return;}
-  e.respondWith(caches.match(req).then(c=>c||fetch(req).then(r=>{const copy=r.clone(); caches.open(CACHE).then(cc=>cc.put(req,copy)); return r;})));});
+self.addEventListener('fetch',e=>{const req=e.request; if(req.mode==='navigate'){e.respondWith(fetch(req).catch(()=>caches.match('./index.html?v=1.5.0')));return;} e.respondWith(caches.match(req).then(c=>c||fetch(req).then(r=>{const copy=r.clone(); caches.open(CACHE).then(cc=>cc.put(req,copy)); return r;})));});
